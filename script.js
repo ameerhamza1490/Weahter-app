7b6c3ede5393a271bfe32e0e141217062902654b
ameerhamza1490 <ameerhamza1490@gmail.com>
2025-03-06 08:00:08 +0500

    Edited

--- script.js
+++ script.js
@@ -1,62 +1,101 @@
 const weatherData = document.querySelector("#weatherData");
 const city = document.querySelector("#city");
+const search = document.querySelector("#search");
+const searchBtn = document.querySelector("#search-btn");
+const currentLocation = document.querySelector("#current-location");
+const APIKEY = `1a208d48d95748f4ac72f02def549960`;
 
-const currentDate = new Date();
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+  const dayName = weekdays[currentDate.getDay()];
+  const monthName = months[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+  const dayNumber = currentDate.getDate();
 
-const dayName = weekdays[currentDate.getDay()];
-const monthName = months[currentDate.getMonth()];
-const year = currentDate.getFullYear();
-const dayNumber = currentDate.getDate();
+  return `${dayName}, ${dayNumber} ${monthName} ${year}.`;
+};
 
-const fullDate = `${dayName}, ${dayNumber} ${monthName} ${year}.`;
-console.log(fullDate);
+const updateWeather = (data) => {
+  const {
+    name,
+    main: { temp, temp_max, temp_min, feels_like, humidity },
+    wind: { speed },
+    weather,
+    clouds: { all },
+  } = data;
+  const weatherCondition = weather[0].main;
+  city.innerHTML = `
+    <h2>${name}</h2>
+    <h3>${getCurrentDate()}</h3>
+    <h1>${Math.round(temp)}°</h1>
+    <p>${weatherCondition}</p>
+    <h2>Precipitation</h2>
+    <h3>Max: ${Math.round(temp_max)}° Min: ${Math.round(temp_min)}°</h3>
+    <h4>Feels Like: ${Math.round(feels_like)}</h4>
+    <div id="columns">
+      <div>Humidity ${humidity}</div>
+      <div>Wind ${speed}km/hr</div>
+      <div>Clouds ${all}%</div>
+    </div>
+  `;
+};
 
-const currentLocation = () => {
+const fetchWeatherData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    updateWeather(data);
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const getWeatherByLocation = (latitude, longitude) => {
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}&units=metric`;
+  fetchWeatherData(weatherAPI);
+};
+
+const getWeatherByCity = () => {
+  const cityName = search.value;
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}&units=metric`;
+  fetchWeatherData(weatherAPI);
+};
+
+const getLocationWeather = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
         const { latitude, longitude } = position.coords;
-        console.log(latitude, longitude);
-        weatherFinder(latitude, longitude);
+        getWeatherByLocation(latitude, longitude);
       },
-      (error) => {
-        console.log(error.message);
-      }
+      (error) => alert(error.message)
     );
   }
 };
 
-const weatherFinder = async (latitude, longitude) => {
-  const APIKEY = `1a208d48d95748f4ac72f02def549960`;
-  const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}&units=metric`;
-  try {
-    const response = await fetch(weatherAPI);
-    const data = await response.json();
-    const { feels_like, humidty, temp, temp_max, temp_min } = data.main;
-    const weatherCondition = data.weather[0].main;
-    console.log(weatherCondition);
-    const cityName = data.name;
-    console.log(data);
-    city.innerHTML = `<h2>${cityName}</h2> <h3> ${fullDate}</h3>  <h1> ${Math.round(temp)}°</h1> <h2> Precipitation </h2> <h3> Max: ${Math.round(temp_max)}° Min: ${Math.round(temp_min)}°</h3>`;
-  } catch (error) {
+getLocationWeather();
 
+searchBtn.addEventListener("click", getWeatherByCity);
+currentLocation.addEventListener("click", getLocationWeather);
+document.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    getWeatherByCity();
   }
-};
-currentLocation();
+});
